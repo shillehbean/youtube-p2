09ce014e212951ca9fb7e6b763600ac02d430970
Mahmood Mustafa Youssef Shilleh <104477522+shillehbean@users.noreply.github.com>
2024-02-10 23:28:46 -0600

    Create mpu9250_basic.py

---
+++ mpu9250_basic.py
@@ -0,0 +1,31 @@
+import time
+from mpu9250_jmdev.registers import *
+from mpu9250_jmdev.mpu_9250 import MPU9250
+
+# Create an MPU9250 instance
+mpu = MPU9250(
+    address_ak=AK8963_ADDRESS,
+    address_mpu_master=MPU9050_ADDRESS_68,  # In case the MPU9250 is connected to another I2C device
+    address_mpu_slave=None,
+    bus=1,
+    gfs=GFS_1000,
+    afs=AFS_8G,
+    mfs=AK8963_BIT_16,
+    mode=AK8963_MODE_C100HZ)
+
+# Configure the MPU9250
+mpu.configure()
+
+while True:
+    # Read the accelerometer, gyroscope, and magnetometer values
+    accel_data = mpu.readAccelerometerMaster()
+    gyro_data = mpu.readGyroscopeMaster()
+    mag_data = mpu.readMagnetometerMaster()
+
+    # Print the sensor values
+    print("Accelerometer:", accel_data)
+    print("Gyroscope:", gyro_data)
+    print("Magnetometer:", mag_data)
+
+    # Wait for 1 second before the next reading
+    time.sleep(1)
