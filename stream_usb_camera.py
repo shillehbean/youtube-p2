62b0950e855579126ed4a1a06f681af8cc9336f2
Mahmood Mustafa Youssef Shilleh <104477522+shillehbean@users.noreply.github.com>
2024-03-30 02:25:35 -0500

    Create stream_usb_camera.py

---
+++ stream_usb_camera.py
@@ -0,0 +1,26 @@
+from flask import Flask, Response
+from picamera2 import Picamera2
+import cv2
+
+### You can donate at https://www.buymeacoffee.com/mmshilleh 
+
+app = Flask(__name__)
+
+camera = Picamera2()
+camera.configure(camera.create_preview_configuration(main={"format": 'XRGB8888', "size": (640, 480)}))
+camera.start()
+
+def generate_frames():
+    while True:
+        frame = camera.capture_array()
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
